6c6eff17cd990af6c9f2d7bbf8b37b2f1b5a050c
Jakub Mokosa <7500146+mokosa@users.noreply.github.com>
2021-10-08 12:24:10 +0200

    add typescript, eslint and base for prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -13,8 +14,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!
+        </a> edit
         </h1>
+
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +68,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
